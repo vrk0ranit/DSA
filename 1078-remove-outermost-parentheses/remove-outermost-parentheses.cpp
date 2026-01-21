aaa2ac9fd80b782a33395b46370c28835b3f8d89
Ranit Bag <118438283+vrk0ranit@users.noreply.github.com>
2026-01-21 22:53:29 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (24.26%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result;
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' && count++>0){
+                result+=s[i];
+            }
+            if(s[i]==')' && count-->1){
+                result+=s[i];
+            }
+        }
+        return result;
+    }
+};
