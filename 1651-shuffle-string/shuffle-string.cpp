1524945654aa32cfb1a5e4194fa6d27171761b18
Ranit Bag <118438283+vrk0ranit@users.noreply.github.com>
2026-01-21 23:50:57 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (50.19%) - LeetSync

---
+++ 1651-shuffle-string/shuffle-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        int n=s.size();
+        string res(n,' ');
+        for(int i=0;i<n;i++){
+            res[indices[i]]=s[i];
+        }
+        return res;
+    }
+};
