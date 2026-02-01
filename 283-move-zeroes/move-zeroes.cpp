b4a6d37013da4f0314653a0c6f5480c6b20873ca
Ranit Bag <118438283+vrk0ranit@users.noreply.github.com>
2026-02-01 20:14:37 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (81.39%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -3,7 +3,7 @@ public:
     void moveZeroes(vector<int>& nums) {
         int left=0;
         for(int right=0;right<nums.size();right++){
-            if(nums[right]!=0){
+            if( nums[right]!=0){
                 swap(nums[left],nums[right]);
                 left++;
             }
