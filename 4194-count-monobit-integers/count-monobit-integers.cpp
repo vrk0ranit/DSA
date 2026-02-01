3c8bb604d9e1d6222406ff9c57c06b457b3629f2
Ranit Bag <118438283+vrk0ranit@users.noreply.github.com>
2026-02-01 15:48:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (25.59%) - LeetSync

--- 4194-count-monobit-integers/count-monobit-integers.cpp
+++ 4194-count-monobit-integers/count-monobit-integers.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int countMonobit(int n) {
-        int count = 1; // 0 is always monobit
-        int val = 1;   // starts with 2^1 - 1 = 1
+        int count = 1;
+        int val = 1;  
         while (val <= n) {
             count++;
-            val = (val << 1) | 1;  // generate next monobit number
+            val = (val << 1) | 1;  
         }
         return count;
     }
