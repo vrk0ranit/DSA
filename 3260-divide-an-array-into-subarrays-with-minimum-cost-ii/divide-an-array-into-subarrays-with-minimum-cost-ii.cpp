c1dededa277984ac09a2a19235eb1833b4d51644
Ranit Bag <118438283+vrk0ranit@users.noreply.github.com>
2026-02-02 17:08:24 +0530

    Time: 395 ms (65.63%) | Memory: 174.6 MB (45.31%) - LeetSync

---
+++ 3260-divide-an-array-into-subarrays-with-minimum-cost-ii/divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
@@ -0,0 +1,80 @@
+class Solution {
+public:
+    struct SmartWindow {
+        int K;
+        multiset<int> low, high;
+        long long sumLow = 0;
+
+        SmartWindow(int k) : K(k) {}
+
+        int windowSize() const {
+            return (int)low.size() + (int)high.size();
+        }
+
+        void rebalance() {
+            int need = min(K, windowSize());
+
+            while ((int)low.size() > need) {
+                auto it = prev(low.end());
+                sumLow -= *it;
+                high.insert(*it);
+                low.erase(it);
+            }
+
+            while ((int)low.size() < need && !high.empty()) {
+                auto it = high.begin();
+                sumLow += *it;
+                low.insert(*it);
+                high.erase(it);
+            }
+        }
+
+        void add(int x) {
+            if (low.empty() || x <= *prev(low.end())) {
+                low.insert(x);
+                sumLow += x;
+            } else {
+                high.insert(x);
+            }
+            rebalance();
+        }
+
+        void remove(int x) {
+            auto itLow = low.find(x);
+            if (itLow != low.end()) {
+                sumLow -= x;
+                low.erase(itLow);
+            } else {
+                auto itHigh = high.find(x);
+                high.erase(itHigh);
+            }
+            rebalance();
+        }
+
+        long long query() const {
+            return sumLow;
+        }
+    };
+
+    long long minimumCost(vector<int>& nums, int k, int dist) {
+        int n = nums.size();
+        k--;                        // nums[0] is always included
+        SmartWindow window(k);
+
+        // initial window [1 ... 1 + dist]
+        for (int i = 1; i <= 1 + dist; i++) {
+            window.add(nums[i]);
+        }
+
+        long long ans = window.query();
+
+        // slide window
+        for (int i = 2; i + dist < n; i++) {
+            window.remove(nums[i - 1]);
+            window.add(nums[i + dist]);
+            ans = min(ans, window.query());
+        }
+
+        return ans + nums[0];
+    }
+};
