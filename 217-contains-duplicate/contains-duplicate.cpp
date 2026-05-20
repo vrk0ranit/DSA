c5549ef2544e8471bd2c1692ba4e73756465fe52
Ranit Bag <118438283+vrk0ranit@users.noreply.github.com>
2026-05-20 12:33:38 +0530

    Time: 47 ms (67.72%) | Memory: 90.8 MB (44.21%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       unordered_set<int> seen;
+       for(int x: nums){
+        if(seen.count(x)) return true;
+        seen.insert(x);
+       }
+       return false;
+    }
+};
